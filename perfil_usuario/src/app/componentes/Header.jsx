d4323917f59ba0998302aff191daff23aaf5bf03
Martin Fuks <martinfukss@gmail.com>
2024-07-03 19:56:08 -0300

    Start

---
+++ perfil_usuario/src/app/componentes/Header.jsx
@@ -0,0 +1,15 @@
+import style from "./Header.module.css"
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div className={style.home}>
+                <div>
+                    
+                </div>
+        </div>
+    )
+}
+
+export default Header;
