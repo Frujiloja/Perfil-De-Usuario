1110e877ef501b4de5604cd4dfcc393a2b714ab8
Martin Fuks <martinfukss@gmail.com>
2024-07-05 16:38:32 -0300

    Final

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Perfil-De-Usuario
-Página web de un perfil de usuario que incluya información básica, una lista de intereses y una sección de contacto.
+Página web de un perfil de usuario que incluya información básica, intereses y una sección de contacto.
+
+## Instalación
+
+1. **Clonar el repositorio:**
+
+   ```bash
+   git clone https://github.com/Frujiloja/Perfil-De-Usuario.git
+   cd perfil-usuario
+
+2. **Abrir Terminal:**
+
+   Abrir la terminal desde la carpeta llamada "perfil_usuario"
+
+3. **Instalar Dependencias:**
+
+   npm install
+
+4. **Ejecutar:**
+
+   npm run dev
+
+   Abrir [http://localhost:3000].
--- perfil_usuario/src/app/componentes/About.jsx
+++ perfil_usuario/src/app/componentes/About.jsx
@@ -16,6 +16,7 @@ const About = () => {
 
   return (
     <div className={styles.about}>
+      <h1 className="Acerca" style={{ fontSize: "6vh", marginBottom: '5vh' }} >Erik Castello.</h1>
       <h1 className="Acerca" style={{ fontSize: "4.5vh" }} >Acerca de mi.</h1>
       <p className="Acerca" >
       Soy un apasionado programador con un estilo de vida activo. Cuando no estoy inmerso en el mundo del código, me encontrarás en el gimnasio esforzándome por superar mis límites físicos o en la cancha de fútbol disfrutando de un buen partido. Combino mi amor por la tecnología con una vida saludable y activa, buscando siempre el equilibrio entre el trabajo y el juego.
